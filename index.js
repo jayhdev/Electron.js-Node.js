eabb4da57ee88e63daa900216bb5a00f8ef1aafb
Smart Coder <smartcoder415@gmail.com>
2019-09-02 07:45:44 -0700

    chore(app): setup lint

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-'use strict';
 const path = require('path');
-const {app, BrowserWindow, Menu} = require('electron');
-/// const {autoUpdater} = require('electron-updater');
-const {is} = require('electron-util');
+const { app, BrowserWindow, Menu } = require('electron');
+// const {autoUpdater} = require('electron-updater');
+const { is } = require('electron-util');
 const unhandled = require('electron-unhandled');
 const debug = require('electron-debug');
 const contextMenu = require('electron-context-menu');
@@ -31,60 +30,62 @@ app.setAppUserModelId('com.company.AppName');
 let mainWindow;
 
 const createMainWindow = async () => {
-	const win = new BrowserWindow({
-		title: app.getName(),
-		show: false,
-		width: 600,
-		height: 400
-	});
+  const win = new BrowserWindow({
+    title: app.getName(),
+    show: false,
+    width: 600,
+    height: 400
+  });
 
-	win.on('ready-to-show', () => {
-		win.show();
-	});
+  win.on('ready-to-show', () => {
+    win.show();
+  });
 
-	win.on('closed', () => {
-		// Dereference the window
-		// For multiple windows store them in an array
-		mainWindow = undefined;
-	});
+  win.on('closed', () => {
+    // Dereference the window
+    // For multiple windows store them in an array
+    mainWindow = undefined;
+  });
 
-	await win.loadFile(path.join(__dirname, 'index.html'));
+  await win.loadFile(path.join(__dirname, 'index.html'));
 
-	return win;
+  return win;
 };
 
 // Prevent multiple instances of the app
 if (!app.requestSingleInstanceLock()) {
-	app.quit();
+  app.quit();
 }
 
 app.on('second-instance', () => {
-	if (mainWindow) {
-		if (mainWindow.isMinimized()) {
-			mainWindow.restore();
-		}
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
+    }
 
-		mainWindow.show();
-	}
+    mainWindow.show();
+  }
 });
 
 app.on('window-all-closed', () => {
-	if (!is.macos) {
-		app.quit();
-	}
+  if (!is.macos) {
+    app.quit();
+  }
 });
 
 app.on('activate', async () => {
-	if (!mainWindow) {
-		mainWindow = await createMainWindow();
-	}
+  if (!mainWindow) {
+    mainWindow = await createMainWindow();
+  }
 });
 
 (async () => {
-	await app.whenReady();
-	Menu.setApplicationMenu(menu);
-	mainWindow = await createMainWindow();
+  await app.whenReady();
+  Menu.setApplicationMenu(menu);
+  mainWindow = await createMainWindow();
 
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
+  const favoriteAnimal = config.get('favoriteAnimal');
+  mainWindow.webContents.executeJavaScript(
+    `document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`
+  );
 })();
