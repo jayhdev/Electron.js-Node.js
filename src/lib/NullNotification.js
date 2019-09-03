c0d373e75dff39c8d2d5e0991cdb0dd741552675
Passion Coder <passioncoder@outlook.com>
2019-09-03 08:04:22 -0700

    feat(notification): null notification

---
+++ src/lib/NullNotification.js
@@ -0,0 +1,24 @@
+const { remote } = require('electron');
+const log = remote.require('electron-log');
+
+class NullNotification {
+  constructor(title, options) {
+    log.info(
+      `Null notification initialized ${title} ${JSON.stringify(options)}`
+    );
+  }
+
+  get permission() {
+    return 'granted';
+  }
+
+  show() {
+    log.info(`Null notification show called`);
+  }
+
+  close() {
+    log.info(`Null notification close called`);
+  }
+}
+
+module.exports = NullNotification;
