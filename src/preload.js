e9fec4b1a32e5c71e2e814040fdc5c18e600eede
Smart Coder <smartcoder415@gmail.com>
2019-09-09 07:30:57 -0700

    feat(ipc-renderer): preload js

---
+++ src/preload.js
@@ -0,0 +1,66 @@
+const { ipcRenderer, shell, remote } = require('electron');
+const Notification = require('./lib/Notification');
+const NullNotification = require('./lib/NullNotification');
+
+window.ElectronNotification = Notification;
+window.NullNotification = NullNotification;
+window.Notification = Notification;
+
+window.electronEnv = remote.getGlobal('electronEnv');
+window.electronOS = process.platform;
+
+const EVENTS = ['unread-changed', 'notification-clicked', 'logged-out'];
+
+EVENTS.forEach(function(e) {
+  window.addEventListener(e, function(event) {
+    ipcRenderer.sendToHost(e, event.detail);
+  });
+});
+
+window.onload = function() {
+  const $ = require('jquery');
+  function checkExternalUrl(e) {
+    const href = $(this).attr('href');
+    const external = $(this).attr('target') === '_blank';
+    // @TODO double check download url
+    // @TODO add download file feature
+    const downloadRegex = new RegExp('/messages/(\\d+)/download');
+
+    const mainDomainRegex = /[\w]+\.[\w]+$/;
+    let mainDomain = location.host.match(mainDomainRegex) || [];
+    mainDomain = mainDomain[0] || location.host;
+
+    // Check href matching current domain
+    if (
+      RegExp(`^${location.protocol}//${location.host}`).test(href) &&
+      !external
+    ) {
+      return;
+    }
+
+    // Check href matching main domain
+    if (
+      RegExp(`^${location.protocol}//${mainDomain}`).test(href) &&
+      !external
+    ) {
+      return;
+    }
+
+    // Check href matching relative URL
+    if (!/^([a-z]+:)?\/\//.test(href)) {
+      if (external && !downloadRegex.exec(href)) {
+        shell.openExternal(`${location.protocol}//${location.host}${href}`);
+        e.preventDefault();
+      }
+
+      return;
+    }
+
+    if (/^https?:\/\/.+/.test(href) && !downloadRegex.exec(href)) {
+      shell.openExternal(href);
+      e.preventDefault();
+    }
+  }
+
+  $(document).on('click', 'a', checkExternalUrl);
+};
