5db60522e284f498d58a108c1c31b80fb7ab1917
Smart Coder <smartcoder415@gmail.com>
2019-10-24 04:03:07 -0700

    feat(app): loading and main screen

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 // const {autoUpdater} = require('electron-updater');
 const { is } = require('electron-util');
 const unhandled = require('electron-unhandled');
 const debug = require('electron-debug');
 const contextMenu = require('electron-context-menu');
-const config = require('./src/config');
+const log = require('electron-log');
+const { afterMainWindow } = require('./background.custom');
 const menu = require('./src/menu');
+
+log.transports.console.level = process.env.DESKTOP_LOG_LEVEL || 'info';
+log.transports.file.level = process.env.DESKTOP_LOG_LEVEL || 'info';
+
+if (process.env.NODE_ENV === 'development') {
+  require('electron-reload')(__dirname);
+}
 
 unhandled();
 debug();
 contextMenu();
 
 // Note: Must match `build.appId` in package.json
-app.setAppUserModelId('com.otctrade.app');
+app.setAppUserModelId('com.pokerswaps.app');
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
@@ -28,23 +36,22 @@ app.setAppUserModelId('com.otctrade.app');
 
 // Prevent window from being garbage collected
 let mainWindow;
+let splashWindow = null;
+let connectionUp = true;
+let splashActive = true;
 
 const createMainWindow = async () => {
   const win = new BrowserWindow({
     title: app.getName(),
+    width: 1200,
+    height: 925,
     show: false,
-    width: 600,
-    height: 400
-  });
-
-  win.on('ready-to-show', () => {
-    win.show();
-  });
-
-  win.on('closed', () => {
-    // Dereference the window
-    // For multiple windows store them in an array
-    mainWindow = undefined;
+    frame: process.platform !== 'win32',
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true,
+      devTools: false
+    }
   });
 
   await win.loadFile(path.join(__dirname, 'src/public/app.html'));
@@ -73,19 +80,63 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', async () => {
-  if (!mainWindow) {
-    mainWindow = await createMainWindow();
-  }
-});
-
 (async () => {
   await app.whenReady();
   Menu.setApplicationMenu(menu);
   mainWindow = await createMainWindow();
 
-  const favoriteAnimal = config.get('favoriteAnimal');
-  mainWindow.webContents.executeJavaScript(
-    `document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`
-  );
+  mainWindow.isIntitialLoaded = false;
+
+  splashWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    frame: false,
+    show: false,
+    resizable: false,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: false
+    }
+  });
+  splashWindow.loadFile(path.join(__dirname, 'src/public/splash.html'));
+  splashWindow.on('ready-to-show', () => {
+    splashWindow.show();
+  });
+
+  ipcMain.on('connection-up', () => {
+    if (!connectionUp && !splashActive) {
+      log.info('Reloading main window since connection is reset');
+      if (mainWindow.destroyTray) {
+        mainWindow.destroyTray();
+      }
+      mainWindow.reload();
+    }
+
+    if (splashActive) {
+      splashWindow.webContents.send('connecting');
+    }
+
+    connectionUp = true;
+  });
+
+  ipcMain.on('connection-down', () => {
+    connectionUp = false;
+    if (splashActive) {
+      splashWindow.webContents.send('disconnected');
+    }
+  });
+
+  ipcMain.on('web-app-loaded', () => {
+    if (connectionUp && splashActive) {
+      splashActive = false;
+      splashWindow.destroy();
+
+      mainWindow.show();
+
+      afterMainWindow(mainWindow);
+    }
+  });
+
+  log.info(`Logging to: ${log.transports.file.file}`);
 })();
